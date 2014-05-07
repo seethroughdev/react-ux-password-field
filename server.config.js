28a6edc82fa25d5a3306df92099160d472e8f8c9
Thomas Deutsch <deutsch.thomas@gmail.com>
2014-05-07 16:28:33 +0200

    enabled harmony

--- server.config.js
+++ server.config.js
@@ -16,7 +16,7 @@ var config = {
         loaders: [
             { test: /\.css/, loader: "style-loader!css-loader" },
 	        { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
-			{ test: /\.js$/, loader: "jsx-loader" },
+			{ test: /\.js$/, loader: "jsx-loader?harmony" },
             { test: /\.png/, loader: "url-loader?limit=100000&mimetype=image/png" },
             { test: /\.gif/, loader: "url-loader?limit=100000&mimetype=image/gif" },
             { test: /\.jpg/, loader: "file-loader" }
