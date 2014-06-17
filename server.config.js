c25c7b01506abda45d999b093a29ae9e1fc7cdbf
Thomas Deutsch <deutsch.thomas@gmail.com>
2014-06-17 14:32:57 +0200

    Update server.config.js

--- server.config.js
+++ server.config.js
@@ -15,8 +15,8 @@ var config = {
     module: {
         loaders: [
             { test: /\.css/, loader: "style-loader!css-loader" },
-	        { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
-			{ test: /\.js$/, loader: "jsx-loader" },
+	    { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
+	    { test: /\.js$/, loader: "jsx-loader?harmony" },
             { test: /\.png/, loader: "url-loader?limit=100000&mimetype=image/png" },
             { test: /\.gif/, loader: "url-loader?limit=100000&mimetype=image/gif" },
             { test: /\.jpg/, loader: "file-loader" }
