2de5ed13d9c81bec89ed7a66041a26af93ace48e
Thomas Deutsch <deutsch.thomas@gmail.com>
2014-06-17 13:36:52 +0200

    Revert "enabled harmony"
    This reverts commit 28a6edc82fa25d5a3306df92099160d472e8f8c9.

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         loaders: [
             { test: /\.css/, loader: "style-loader!css-loader" },
 	        { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
-			{ test: /\.js$/, loader: "jsx-loader?harmony" },
+			{ test: /\.js$/, loader: "jsx-loader" },
             { test: /\.png/, loader: "url-loader?limit=100000&mimetype=image/png" },
             { test: /\.gif/, loader: "url-loader?limit=100000&mimetype=image/gif" },
             { test: /\.jpg/, loader: "file-loader" }
