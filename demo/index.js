fbe7ed7bcc2de11573d4119711b6fdfdc7976843
Adam L <github@adamin.net>
2015-02-15 10:26:31 -0800

    Added demo style

--- demo/index.js
+++ demo/index.js
@@ -3,4 +3,7 @@
 var React         = require('react'),
     InputPassword = require('../src/js/react-password-field');
 
+require('normalize.css/normalize.css')
+require('./style.scss');
+
 React.render(<InputPassword />, document.getElementById('content'));
