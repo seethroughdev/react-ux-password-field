2bd4e33bb5220bb491787c5990ecf9394f625975
Adam L <github@adamin.net>
2015-02-16 23:37:43 -0800

    Cleaned up index

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var React         = require('react'),
-    InputPassword = require('../../src/js/react-password-field');
-
 require('normalize.css/normalize.css')
 require('./style.scss');
+
+
+
+var React         = require('react'),
+    InputPassword = require('../../src/js/react-password-field');
 
 React.render(
   <form>
