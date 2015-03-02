43b0454e3a4f1c0b2d834f970a14b6b66ba3802c
Adam L <github@adamin.net>
2015-03-01 23:46:59 -0800

    Separated demo css out for reference

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,16 +1,16 @@
 'use strict';
 
-// default css
+// demo pages css
 require('normalize.css/normalize.css')
 require('./vendor/skeleton.css');
 require('./vendor/github.css');
-
-// syntax highlighting
 require('./vendor/syntax.css');
 require('./vendor/rainbow-custom.min.js');
-
-// custom css
 require('./css/style.scss');
+
+// css for form field
+require('./css/demo.scss');
+
 
 /*==========  APP  ==========*/
 
