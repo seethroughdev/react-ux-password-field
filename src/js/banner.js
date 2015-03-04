01d4cd6999604691d8b12a3549cea4f1ec03dbb9
Adam L <github@adamin.net>
2015-03-03 21:56:03 -0800

    Recompiled with updated version

--- src/js/banner.js
+++ src/js/banner.js
@@ -1,9 +1,12 @@
 'use strict';
 
+var version = require('root-require')('package.json').version
+
 var banner;
-banner = 'React UX Password Field\n\n';
+banner = 'React UX Password Field\n';
+banner += 'version: ' + version + '\n\n';
+banner += 'MIT Licensed\n'
 banner += 'github: https://github.com/seethroughtrees/react-ux-password-field/\n'
 banner += 'demo: https://seethroughtrees.github.io/react-ux-password-field/\n'
-banner += 'issues: https://github.com/seethroughtrees/react-ux-password-field/issues\n'
 
 module.exports = banner;
